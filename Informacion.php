8ff99bcdcb28ad2f5e12d1bbf2f695a991669fc2
Marie Lopez <48026210+Marefw@users.noreply.github.com>
2019-05-06 22:10:53 -0500

    Delete Informacion.php

--- Informacion.php
+++
@@ -1,8 +0,0 @@
-<?php
-   $destino="maryjockl@gmail.com";
-   $nombre = $_POST["nombre"];
-   $email = $_POST["email"];
-   $contenido = "Nombre: " . $Nombre . "\nEmail: " .$Email;
-   mail($destino,"Contacto", $contenido);
-   header("Location:graciass.html");
-?>
